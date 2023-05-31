1db0f11301feb1347ea4e086f937e3129eb90934
Andy <quinette.pro@gmail.com>
2023-05-31 17:08:22 +0200

    ca avance

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { FormControl } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,18 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'projet-my-yuka';
+
+  public myTitle: string;
+  public mySlogan: string;
+  
+  champSaisi = new FormControl('');
+
+  constructor() {
+    this.myTitle = "My Yuka";
+    this.mySlogan = "Obtenez les informations sur les produits alimentaires"
+  }
+
+  public afficherChampSaisi() {
+    alert(this.champSaisi.value)
+  }
 }
