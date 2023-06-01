6ac056fce59de7f3debc1a76279dad9bb758de9c
Andy <quinette.pro@gmail.com>
2023-06-01 11:16:59 +0200

    app fonctionnel. CSS et responsive a faire

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,3 +1,4 @@
+import { HttpClient } from '@angular/common/http';
 import { Component } from '@angular/core';
 import { FormControl } from '@angular/forms';
 
@@ -11,15 +12,44 @@ export class AppComponent {
 
   public myTitle: string;
   public mySlogan: string;
-  
+
   champSaisi = new FormControl('');
 
-  constructor() {
+  public photo: string;
+  public ingredient: string;
+  public allergen: string;
+  public nova: string;
+  public nutriscore: string;
+  public palm: string;
+  public info: string;
+
+
+
+  constructor(private http: HttpClient) {
     this.myTitle = "My Yuka";
-    this.mySlogan = "Obtenez les informations sur les produits alimentaires"
+    this.mySlogan = "Obtenez les informations sur les produits alimentaires";
+    this.photo = "";
+    this.ingredient = "";
+    this.allergen = "";
+    this.nova = "";
+    this.nutriscore = "";
+    this.palm = "";
+    this.info = "";
   }
 
+
+
   public afficherChampSaisi() {
-    alert(this.champSaisi.value)
+    return this.http.get<any>('https://world.openfoodfacts.org/api/v0/product/' + this.champSaisi.value)
+      .subscribe(data => {
+        console.log(data)
+        this.photo = data.product.image_url
+        this.ingredient =  data.product.ingredients_text_fr
+        this.allergen =  data.product.allergens_from_ingredients
+        this.nova =  data.product.nova_group
+        this.nutriscore =  data.product.nutrition_grade_fr
+        this.palm =  data.product.ingredients_from_palm_oil_n
+        this.info =  data.product.stores
+      })
   }
 }
